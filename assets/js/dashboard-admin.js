5dde13727d727712442c6940165b8549ce6956b5
alanprayogo <alanbp98@gmail.com>
2024-01-29 02:33:53 +0700

    modal-data done

--- assets/js/dashboard-admin.js
+++ assets/js/dashboard-admin.js
@@ -44,3 +44,123 @@ $(document).ready(function () {
 });
 
 
+// add-admin
+const saveButton = document.querySelector(".modal-footer button.btn-primary");
+
+// Menambahkan event listener untuk meng-handle klik tombol "Save"
+saveButton.addEventListener("click", function () {
+  // Menutup modal
+  $("#add-admin-Modal").modal("hide");
+
+  // Menampilkan alert success
+  const alertSuccess = `
+                              <div class="alert alert-success alert-dismissible fade show w-25 position-fixed top-0 start-50 translate-middle mt-5" role="alert">
+                                Data saved successfully
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                              </div>
+                            `;
+
+  // Menambahkan alert success ke dalam body
+  document.body.insertAdjacentHTML("beforeend", alertSuccess);
+
+  // Menghilangkan alert setelah 3 detik
+  setTimeout(function () {
+    $(".alert").alert("close");
+  }, 4000);
+});
+
+// delete-admin
+const deleteButton = document.querySelector(
+  ".modal-footer button.btn-danger"
+);
+
+// Menambahkan event listener untuk meng-handle klik tombol "Delete"
+deleteButton.addEventListener("click", function () {
+  // Menutup modal
+  $("#delete-admin-Modal").modal("hide");
+
+  // Menampilkan alert danger dengan latar belakang yang berbeda
+  const alertDanger = `
+    <div class="alert alert-danger alert-dismissible bg-danger fade show w-25 position-fixed top-0 start-50 translate-middle mt-5" role="alert">
+      Data deleted successfully
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+
+  // Menambahkan alert danger ke dalam body
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    alertDanger
+  );
+
+  // Menghilangkan alert setelah 3 detik
+  setTimeout(function () {
+    $(".alert").alert("close");
+  }, 4000);
+});
+
+// update-admin
+const updateButton = document.querySelector(
+  ".modal-footer button.btn-warning"
+);
+
+// Menambahkan event listener untuk meng-handle klik tombol "Update"
+updateButton.addEventListener(
+  "click",
+  function () {
+    // Menutup modal
+    $("#update-admin-Modal").modal("hide");
+
+    // Menampilkan alert warning
+    const alertWarning = `
+    <div class="alert alert-warning alert-dismissible bg-warning fade show w-25 position-fixed top-0 start-50 translate-middle mt-5" role="alert">
+      Data updated successfully
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+
+    // Menambahkan alert warning ke dalam body
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      alertWarning
+    );
+
+    // Menghilangkan alert setelah 3 detik
+    setTimeout(function () {
+      $(".alert").alert("close");
+    }, 4000);
+  }
+);
+
+// update-user
+const updateuserButton = document.querySelector(
+  ".user button.btn-warning"
+);
+
+// Menambahkan event listener untuk meng-handle klik tombol "Update"
+updateuserButton.addEventListener(
+  "click",
+  function () {
+    // Menutup modal
+    $("#update-user-Modal").modal("hide");
+
+    // Menampilkan alert warning
+    const alertWarning = `
+    <div class="alert alert-warning alert-dismissible bg-warning fade show w-25 position-fixed top-0 start-50 translate-middle mt-5" role="alert">
+      Data updated successfully
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+
+    // Menambahkan alert warning ke dalam body
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      alertWarning
+    );
+
+    // Menghilangkan alert setelah 3 detik
+    setTimeout(function () {
+      $(".alert").alert("close");
+    }, 4000);
+  }
+);
